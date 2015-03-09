6502153f81c89ae8e4e6e9bb177abcd5a04c9e8c
Maxiee <maxieewong@gmail.com>
2015-03-09 22:47:26 +0800

    init actionbar in MainActivity.java

--- app/src/main/java/com/judymax/materialdrawerlayout/ui/MainActivity.java
+++ app/src/main/java/com/judymax/materialdrawerlayout/ui/MainActivity.java
@@ -1,19 +1,38 @@
 package com.judymax.materialdrawerlayout.ui;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.judymax.materialdrawerlayout.R;
+import com.judymax.materialdrawerlayout.ui.fragment.NavigationDrawerFragment;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    protected Toolbar mToolbar;
+    public ActionBarHelper mActionBar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationDrawerFragment mNavigationDrawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        // 出现 Title 左边的小箭头
+        mActionBar = new ActionBarHelper();
+        mActionBar.init();
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
+                .findFragmentById(R.id.navigation_drawer);
     }
 
 
@@ -37,5 +56,29 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ActionBarHelper {
+        private final ActionBar mActionBar;
+        private CharSequence mDrawerTitle;
+        private CharSequence mTitle;
+
+        ActionBarHelper() {
+            mActionBar = getSupportActionBar();
+        }
+
+        public void init() {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar.setDisplayShowHomeEnabled(false);
+            mTitle = mDrawerTitle = getTitle();
+        }
+
+        public void onDrawerClosed() {
+            mActionBar.setTitle(mTitle);
+        }
+
+        public void onDrawerOpened() {
+            mActionBar.setTitle(mDrawerTitle);
+        }
     }
 }
